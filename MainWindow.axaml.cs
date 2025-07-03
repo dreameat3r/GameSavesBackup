6f591b990b563d5f50dd771e88a21cb46058f985
dreameat3r <wildlynx14425@gmail.com>
2025-07-03 07:44:58 -0600

    The prototype of main window was made

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+
+namespace GameSavesBackup;
+
+public partial class MainWindow : Window
+{
+
+    public string Greeting => "Hello world!";
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+        Presets.ItemsSource = new string[]
+        {"Elden Ring", "Dark Souls 2", "Project Zomboid"};
+    }
+}
